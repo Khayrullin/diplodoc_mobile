75a6127980bbd89905a35aff52c8448e3431efec
Bulat Khayrullin <habar.2704@mail.ru>
2019-04-23 14:23:34 +0300

    Валидация полей

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from '../../services/authentication.service';
-import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import {Validators, FormBuilder, FormGroup, FormControl, AbstractControl} from '@angular/forms';
 
 
 @Component({
@@ -9,8 +9,8 @@ import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms'
     styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-    email = '';
-    password = '';
+    email = AbstractControl;
+    password = AbstractControl;
     loginForm: FormGroup;
 
     constructor(private authService: AuthenticationService) {
@@ -18,8 +18,18 @@ export class LoginPage implements OnInit {
             email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(4)]),
             password: new FormControl('', [Validators.required]),
         });
-
     }
+
+    validation_messages = {
+        'email': [
+            { type: 'required', message: 'Поле E-mail обязательно для заполнения.' },
+            { type: 'minlength', message: 'E-mail должен быть длиннее 3х символов' },
+            { type: 'email', message: 'Некорректный E-mail ' },
+        ],
+        'password': [
+            { type: 'required', message: 'Поле Пароль обязательно для заполнения' }
+        ],
+    };
 
     ngOnInit() {
     }
